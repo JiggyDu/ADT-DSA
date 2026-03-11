a9c588177fed2b437ceee2e4374c6bdb3e2a0616
JiggyDu <19100184@usc.edu.ph>
2026-03-11 10:11:52 +0800

    Update var1.c

--- arraylist/var1.c
+++ arraylist/var1.c
@@ -6,8 +6,7 @@ typedef struct {
     int count;
 } List;
 
-
-void List initialize();
+List initialize();
 int insertPos(List *L, int data, int position);
 void deletePos(List *L, int position);
 int locate(List *L, int data);
@@ -16,95 +15,116 @@ void display(List *L);
 
 int main()
 {
-List L;
+    List L = initialize();
 
-L = insertPos(L,0,10);
+    insertPos(&L,10,0);
+    insertPos(&L,20,1);
+    insertPos(&L,10,1);
+    insertPos(&L,10,2);
 
-L = insertPos(L,1,20);
+    display(&L);
 
-L = insertPos(L,1,10);
-
-L = insertPos(L,2,10);
-
-
-
+    return 0;
 }
 
-void List initialize()
+List initialize()
 {
-List L;
-L.count = 0;
-for (int i = 0; i < size; i++)
-{
-    L.elem[i] = -1;
+    List L;
+    L.count = 0;
+
+    for(int i = 0; i < size; i++)
+    {
+        L.elem[i] = -1;
+    }
+
+    return L;
 }
 
-return L;
-}
-
-int List insertPos( List *L, int data, int position)
+int insertPos(List *L, int data, int position)
 {
-if (L->count >= size )
-{
-    printf("Size is full");
-}
+    if (L->count >= size)
+    {
+        printf("Size is full\n");
+        return 0;
+    }
 
-if (position < 0 || position > L->count)
-{
-    printf("Invalid Position");
-}
-
- for (int i = L->count; i > position; i--)
-{
-    L->elem[i] = L->elem[i - 1];
-}
-
-L -> elem[position] = data;
-L-> count++;
-return 1;
-}
-
-
-void List deletePos(List *L, int position)
-{
     if (position < 0 || position > L->count)
-{
-    printf("Invalid Position");
+    {
+        printf("Invalid Position\n");
+        return 0;
+    }
+
+    for (int i = L->count; i > position; i--)
+    {
+        L->elem[i] = L->elem[i - 1];
+    }
+
+    L->elem[position] = data;
+    L->count++;
+
+    return 1;
 }
 
-for (int i = position; i < L->count - 1; i++)
+void deletePos(List *L, int position)
 {
-    L->elem[i] = L->elem[i + 1];
-}
-L -> count--;
+    if (position < 0 || position >= L->count)
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    for (int i = position; i < L->count - 1; i++)
+    {
+        L->elem[i] = L->elem[i + 1];
+    }
+
+    L->count--;
 }
 
 int locate(List *L, int data)
 {
-for (int i = 0; i < L->count; i++)
-{
-    if (L->element[i] == data)
+    for (int i = 0; i < L->count; i++)
     {
-    return i;
+        if (L->elem[i] == data)
+        {
+            return i;
+        }
     }
-}
-return -1;
+
+    return -1;
 }
 
-void List insertSorted (List *L, int data)
+void insertSorted(List *L, int data)
 {
+    int i;
 
+    if (L->count >= size)
+    {
+        printf("List Full\n");
+        return;
+    }
+
+    for(i = L->count - 1; i >= 0 && L->elem[i] > data; i--)
+    {
+        L->elem[i+1] = L->elem[i];
+    }
+
+    L->elem[i+1] = data;
+    L->count++;
 }
 
-void display (List *L)
+void display(List *L)
 {
-if (L->count == 0)
-{
-    printf("Empty");
-}
+    if (L->count == 0)
+    {
+        printf("Empty\n");
+        return;
+    }
 
-for (int i = 0; i < L->count; i++) {
+    for (int i = 0; i < L->count; i++)
+    {
         printf("%d ", L->elem[i]);
     }
+
     printf("\n");
 }
